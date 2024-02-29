617bc78d3efc72ef2d516126867d0c44899b52b5
matheusPierro <matpierro570@gmail.com>
2024-02-29 12:00:54 -0300

    feat: restart a new project to use a new version .net8

--- Library/Program.cs
+++ Library/Program.cs
@@ -1,11 +1,15 @@
 ﻿using LibraryService.Models;
 
-Console.WriteLine("Execução de cada classe...");
+Console.WriteLine("Starting Program...");
+Console.WriteLine("Initiate Classes");
 
-var address = new Address();// Colocar os atributos
-var autor = new Autor();// Colocar os atributos
-var book = new Book();
-var category = new Category();// Colocar os atributos
-var person = new Person();// Colocar os atributos
-var rental = new Rental();
 var unit = new Unit();
+var address = new Address();
+var person = new Person();
+var autor = new Autor();
+var book = new Book();
+var category = new Category();
+var rental = new Rental();
+
+Console.WriteLine("Starting Program...");
+
